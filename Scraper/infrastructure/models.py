e82410593106e7f021a8efbb88d5e0dbec7bb6a2
Igorello74 <sidneff.igor@ya.ru>
2024-11-29 00:47:10 +0500

    Make use of the redis_om in models

--- Scraper/infrastructure/models.py
+++ Scraper/infrastructure/models.py
@@ -1,15 +1,21 @@
-from dataclasses import dataclass
-from datetime import date
+from datetime import date, datetime, timezone
+from functools import partial
+from typing import Optional
+
+from redis_om import Field, HashModel
 
 
-@dataclass
-class Article:
+ISSN_REGEX = r"^[0-9]{4}-[0-9]{3}[0-9X]$"
+
+
+class Article(HashModel):
     title: str
-    doi: str
-    author: str
-    publisher: str
-    issued: date
-    journal: str
-    url: str
-    print_issn: str
-    electronic_issn: str
+    entity_created: datetime = Field(default_factory=partial(datetime.now, timezone.utc))
+    doi: Optional[str] = Field(index=True)
+    author: Optional[str]
+    publisher: Optional[str]
+    issued: Optional[date] = Field(index=True)
+    journal: Optional[str]
+    url: Optional[str]
+    print_issn: Optional[str] = Field(regex=ISSN_REGEX)
+    electronic_issn: Optional[str] = Field(regex=ISSN_REGEX)
