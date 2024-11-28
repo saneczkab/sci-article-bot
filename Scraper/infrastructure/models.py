a1e701481349248904acad6014fe3c8ff647ff19
Igorello74 <sidneff.igor@ya.ru>
2024-11-29 02:35:46 +0500

    Add all the rest models

--- Scraper/infrastructure/models.py
+++ Scraper/infrastructure/models.py
@@ -1,21 +1,38 @@
 from datetime import date, datetime, timezone
-from functools import partial
 from typing import Optional
 
-from redis_om import Field, HashModel
-
+from redis_om import EmbeddedJsonModel, Field, JsonModel
 
 ISSN_REGEX = r"^[0-9]{4}-[0-9]{3}[0-9X]$"
 
 
-class Article(HashModel):
+class Journal(EmbeddedJsonModel):
     title: str
-    entity_created: datetime = Field(default_factory=partial(datetime.now, timezone.utc))
+    print_issn: Optional[str] = Field(regex=ISSN_REGEX)
+    electronic_issn: Optional[str] = Field(regex=ISSN_REGEX)
+
+
+class Article(EmbeddedJsonModel):
+    title: str
+    entity_created: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     doi: Optional[str] = Field(index=True)
     author: Optional[str]
     publisher: Optional[str]
     issued: Optional[date] = Field(index=True)
-    journal: Optional[str]
+    journal: Optional[Journal]
     url: Optional[str]
-    print_issn: Optional[str] = Field(regex=ISSN_REGEX)
-    electronic_issn: Optional[str] = Field(regex=ISSN_REGEX)
+
+
+class Query(EmbeddedJsonModel):
+    text: str
+    last_search: date
+    new_articles: list[Article]
+
+
+class User(JsonModel):
+    id: int = Field(primary_key=True)
+    queries: list[Query]
+    shown_articles_dois: list[str]
+
+    class Meta:
+        model_key_prefix = "User"
