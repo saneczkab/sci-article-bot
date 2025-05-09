d74bfc7bf348f79ed02d8194d0eea8cb10c53221
Alexander Iarmoshenko <a2398253@gmail.com>
2025-05-09 08:49:10 +0500

    Create C# Filter

---
+++ TelegramBot/TelegramBot/Filter.cs
@@ -0,0 +1,20 @@
+﻿using Bot.Models;
+
+namespace Bot.TelegramBot;
+
+public static class Filter
+{
+    private static readonly HashSet<string> ApprovedIssns = new();
+
+    static Filter()
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "assets", "issns.txt");
+        foreach (var line in File.ReadLines(path))
+            ApprovedIssns.Add(line.Trim());
+    }
+
+    public static IEnumerable<Article> FilterArticles(IEnumerable<Article> articles) => articles.Where(article =>
+        article.Journal is not null &&
+        ((article.Journal.PrintIssn is not null && ApprovedIssns.Contains(article.Journal.PrintIssn)) ||
+         (article.Journal.ElectronicIssn is not null && ApprovedIssns.Contains(article.Journal.ElectronicIssn))));
+}
